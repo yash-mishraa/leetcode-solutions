3aa579268f014bcff1771762b517a02432cd0eb4
Yash Mishra <151184953+yash-mishraa@users.noreply.github.com>
2026-06-23 12:38:16 +0530

    Time: 7 ms (91.33%) | Memory: 19.5 MB (28.59%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -1,10 +1,3 @@
-"""class Solution:
-    def isAnagram(self, s: str, t: str) -> bool:
-        if len(s)!=len(t):
-            return False
-        a=sorted(s)
-        b=sorted(t)
-        return a==b"""
 from collections import Counter
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
