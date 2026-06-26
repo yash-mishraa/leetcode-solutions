78be9fc01b208a8548837a6cb9cbd41bdab455ef
Yash Mishra <151184953+yash-mishraa@users.noreply.github.com>
2026-06-26 22:47:50 +0530

    Time: 723 ms (37.84%) | Memory: 32.5 MB (89.19%) - LeetSync

--- 4075-count-subarrays-with-majority-element-ii/count-subarrays-with-majority-element-ii.py
+++ 4075-count-subarrays-with-majority-element-ii/count-subarrays-with-majority-element-ii.py
@@ -1,45 +1,34 @@
 from typing import List
 
-class Fenwick:
-    def __init__(self, n):
-        self.bit = [0] * (n + 1)
-
-    def update(self, i, delta):
-        while i < len(self.bit):
-            self.bit[i] += delta
-            i += i & -i
-
-    def query(self, i):
-        s = 0
-        while i > 0:
-            s += self.bit[i]
-            i -= i & -i
-        return s
-
 class Solution:
     def countMajoritySubarrays(self, nums: List[int], target: int) -> int:
         n = len(nums)
 
-        offset = n + 2                 
-        size = 2 * n + 5
-        bit = Fenwick(size)
+        offset = n + 2
+        bit = [0] * (2 * n + 5)
 
-        prefix = 0
         ans = 0
+        prefix = 0
 
-        bit.update(prefix + offset, 1)
+        idx = offset
+        while idx < len(bit):
+            bit[idx] += 1
+            idx += idx & -idx
+
+        m = len(bit)
 
         for x in nums:
-            if x == target:
-                prefix += 1
-            else:
-                prefix -= 1
-
+            prefix += 1 if x == target else -1
             idx = prefix + offset
-
-            ans += bit.query(idx - 1)
-
-            bit.update(idx, 1)
+            s = 0
+            i = idx - 1
+            while i:
+                s += bit[i]
+                i -= i & -i
+            ans += s
+            i = idx
+            while i < m:
+                bit[i] += 1
+                i += i & -i
 
         return ans
-        
